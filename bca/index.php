07ce857427d8f873253a649e1d59428c0c6ff16b
alfathin21 <32984275+alfathin21@users.noreply.github.com>
2018-12-19 21:05:03 +0700

    Aplikasi antrian Bank BCA ->Gagal Belum Beres

---
+++ bca/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" href="asset/bootstrap.min.css">
+	<link rel="stylesheet" href="asset/style.css">
+</head>
+<body>
+	<br>
+
+	<div class="container-fluid">
+		<div class="row jarak">
+			<div class="col-md-2">
+				<img class="logo" src="asset/img/logo.png">
+			</div>
+		</div>
+		<div class="garis">
+			<br>
+			<br>
+			<h4 class="text-center text"><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Silahkan ambil nomor antrian  dengan menekan salah satu tombol </b></h4>
+			
+			<br>
+			<div class="row ">
+				<div class="col-md-3"></div>
+				<div class="col-md-4">
+					<a href="">
+						<img class="btn" src="asset/img/btn1.jpg">
+					</a>
+				</div>
+
+				<div class="col-md-5">
+					<a href="">
+						<img class="btn" src="asset/img/btn2.jpg">
+					</a>
+				</div>
+				<div class="col-md-2"></div>
+			</div>
+			<br>
+			<br>
+			<br>
+		</div>
+	</div>
+
+	<script src="asset/jquery-3.3.1.slim.min.js"></script>
+	<script src="asset/popper.min.js"></script>
+	<script src="asset/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ bca/tampil.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Monitoring</title>
+	<link rel="stylesheet" href="asset/bootstrap.min.css">
+</head>
+<body style="background-color:#ecebeb;">
+	<nav class="navbar navbar-dark" style="background-image: url('asset/img/nav.jpg'); background-size:cover ;  height:110px;">
+		<a class="navbar-brand" href="#">
+			<img src="asset/img/logo.png" style=" opacity: 0;" width="120" height="80" class="d-inline-block align-top" >
+		</a>
+	
+	</nav>
+
+	<div class="container-fluid" style="margin-top: 5px;">
+		<div class="row">
+			<div class="col-md-6">
+				<div style="border: 12px solid #0054a6;">
+					<br>
+					<br>
+					<br>
+					<br>
+				</div>
+			</div>
+			<div class="col-md-6" >
+				<div style="background-color:#0054a6;">
+					
+					<img style="padding-left:120px" src="asset/img/ats.jpg" width="580px">
+					<br>
+					<br>
+					<div class="row" style="padding-left:40px;padding-right:40px">
+						<div style="height: 100px; background-color:#ecebeb; " class="col-md-5">
+							<img style="margin-left: -15px;" width="250px" src="asset/img/tampilan.jpg">
+						</div>
+						<div class="col-md-2"></div>
+						<div style="background-color:#ecebeb; " class="col-md-5">
+							<img style="margin-left: -15px;" width="250px" src="asset/img/tampilan2.jpg">
+						</div>
+					</div>
+				<br>
+					<div  class="row" style="padding-left:40px;padding-right:40px">
+						<div style=" height: 100px; background-color:#ecebeb; " class="col-md-5">
+							<img style="margin-left: -15px;" width="250px" src="asset/img/tampilan3.jpg">
+							<br>
+							<br>
+						</div>
+						<div class="col-md-2"></div>
+						<div style="background-color:#ecebeb; " class="col-md-5">
+							<img style="margin-left: -15px;" width="250px" src="asset/img/tampilan4.jpg">
+							<br>
+							<br>
+						</div>
+					</div>
+					<br>
+					
+				</div>
+
+
+			</div>
+
+		</div>
+		
+		<div class="row" style="margin-top: 5px;">
+			<div class="col-md-6">
+				<div style="border: 12px solid #0054a6;">
+					<br>
+					<br>
+					<br>
+					<br>
+				</div>
+			</div>
+			<div class="col-md-6" >
+				<div style="background-color:#0054a6;">
+					
+					<img style="padding-left:120px" src="asset/img/custom.jpg" width="580px">
+					<br>
+					<br>
+					<div class="row" style="padding-left:40px;padding-right:40px">
+						<div style="height: 100px; background-color:#ecebeb; " class="col-md-5">
+							<img style="margin-left: -15px;" width="250px" src="asset/img/custom1.jpg">
+						</div>
+						<div class="col-md-2"></div>
+						<div style="background-color:#ecebeb; " class="col-md-5">
+							<img style="margin-left: -15px;" width="250px" src="asset/img/custom2.jpg">
+							
+						</div>
+					</div>
+					<br>
+				
+					
+					
+					
+				</div>
+
+			</div>
+			
+		</div>
+
+	</div>
+	<!-- <footer>sasa</footer> -->
+	<script src="asset/jquery-3.3.1.slim.min.js"></script>
+	<script src="asset/popper.min.js"></script>
+	<script src="asset/bootstrap.min.js"></script>
+</body>
+</html>
